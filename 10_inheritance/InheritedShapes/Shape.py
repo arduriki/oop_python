d69ac0525cfa9c11f15bcb04a0623b7edb961725
arduriki <jordi.ardura.professional@gmail.com>
2023-08-15 10:49:17 +0200

    Abstract class

--- 10_inheritance/InheritedShapes/Shape.py
+++ 10_inheritance/InheritedShapes/Shape.py
@@ -1,6 +1,9 @@
-# Shape class - basic
+# Shape class
+#
+# To be used as a base class for other classes
 
 import random
+from abc import ABC, abstractmethod
 
 # Set up the colors
 RED = (255, 0, 0)
@@ -8,7 +11,7 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 
-class Shape():  # This is an abstract class
+class Shape(ABC):  # indentifies this as an abstract base class
 
     def __init__(self, window, shapeType, maxWidth, maxHeight):
         self.window = window
@@ -19,3 +22,15 @@ class Shape():  # This is an abstract class
 
     def getType(self):
         return self.shapeType
+
+    @abstractmethod
+    def clickedInside(self, mousePoint):
+        raise NotImplementedError
+
+    @abstractmethod
+    def getArea(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def draw(self):
+        raise NotImplementedError
