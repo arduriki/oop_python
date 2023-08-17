365d86259ea086075c790587ea215fb7c948b7cb
arduriki <jordi.ardura.professional@gmail.com>
2023-08-17 15:14:55 +0200

    Object lifetime

---
+++ 11_memory_management/ReferenceCount.py
@@ -0,0 +1,19 @@
+# Reference count example
+import sys
+
+
+class Square():
+    def __init__(self, width, color):
+        self.width = width
+        self.color = color
+
+
+# Instantiate an object
+oSquare1 = Square(5, 'red')
+print('Reference count is', sys.getrefcount(oSquare1))
+# Reference count of the Square object is 1
+
+# Now set another variable to the same object
+oSquare2 = oSquare1
+print(oSquare2)
+# Reference count of the Square object is 2
